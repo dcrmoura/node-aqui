52afbae812472e8a55fdda61dfe623ac38c738ca
denise moura <denise.cm94@gmail.com>
2024-10-06 00:41:25 -0300

    subindo alterações pelo prompt de comando

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-"console.log('Hello Node.js!');" 
+import express from 'express';
+
+const app = express();
+const porta = 3000;
+
+// rota básica
+app.get('/', (req, res) => {
+    res.send('Olá, mundo!');
+});
+
+// iniciar o servidor
+app.listen(porta, () => {
+    console.log(`Servidor rodando em http://localhost:${porta}`);
+});
---
+++ notes.md
@@ -0,0 +1,2 @@
+# ao abrir o vs, dê um "git pull origin main" para garantir que você tenha as últimas alterações do repositório remoto
+git pull origin main
